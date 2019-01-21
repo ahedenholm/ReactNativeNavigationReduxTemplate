8338acbdb886b604804ad0d43a6eac0caee4bce2
Anders HEDENHOLM <anders.hedenholm@soprasteria.com>
2019-01-21 09:39:11 +0100

    Removed obsolete imports

--- src/Origin.js
+++ src/Origin.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Navigator from './Navigator';
-import Login from './screens/Login';
-import WorkOrders from './screens/WorkOrders';
-import WorkOrderDetails from './screens/WorkOrderDetails';
-import ReportStatus from './screens/ReportStatus';
-import ReportSubmission from './screens/ReportSubmission';
 
 class Origin extends React.Component {
   render() {
